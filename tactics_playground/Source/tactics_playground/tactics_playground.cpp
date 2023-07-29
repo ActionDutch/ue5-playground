313c86937cd5e17625fddc7caf6f289f8e2b36fd
Kyle Netherland <kylenetherland.programming@gmail.com>
2023-07-29 09:22:38 -0400

    Initial lfs setup
    project a gogo

---
+++ tactics_playground/Source/tactics_playground/tactics_playground.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "tactics_playground.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, tactics_playground, "tactics_playground" );
---
+++ tactics_playground/Source/tactics_playground/tactics_playgroundGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "tactics_playgroundGameModeBase.h"
+
