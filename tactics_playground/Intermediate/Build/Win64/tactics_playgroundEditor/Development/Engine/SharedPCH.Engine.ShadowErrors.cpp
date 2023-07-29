d491e58132a2031f13ff2f4301ee1be80a6cacb1
Kyle Netherland <kylenetherland.programming@gmail.com>
2023-07-29 12:01:20 -0400

    Cleaning up

--- tactics_playground/Intermediate/Build/Win64/tactics_playgroundEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.35.32215
-#include "C:/Users/kylen/OneDrive/Documents/Unreal Projects/ue5-playground/tactics_playground/Intermediate/Build/Win64/tactics_playgroundEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- tactics_playground/Source/tactics_playground/tactics_playground.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "tactics_playground.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, tactics_playground, "tactics_playground" );
