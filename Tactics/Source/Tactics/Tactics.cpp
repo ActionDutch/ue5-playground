be1bec63334d575ea7bbe06a30ae2e46bb1db976
Kyle Netherland <kylenetherland.programming@gmail.com>
2024-07-31 16:06:52 -0400

    RESET
    refresh the repo, we dont like these games anymore

--- Tactics/Source/Tactics/Tactics.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Tactics.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tactics, "Tactics" );
